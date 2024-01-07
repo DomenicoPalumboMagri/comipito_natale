d5c46d0d6667cc73380f732a75f26d7bd06b4c06
DomenicoPalumboMagri <palumbo.magri.d@iisgalvanimi.edu.it>
2024-01-07 17:07:53 +0000

    aggiunta bootstrat

--- EsercizioNatale/src/app/app.module.ts
+++ EsercizioNatale/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SecondoComponent } from './secondo/secondo.component';
+import { SampleComponent } from './sample/sample.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SecondoComponent
+    SecondoComponent,
+    SampleComponent
   ],
   imports: [
     BrowserModule
